b37ab3d33b45a9ec1e02414295fe54d203351299
GuillaumePavic <guillaume.pavic.56@gmail.com>
2021-08-03 11:54:21 +0200

    corrected routes

--- app/router/router.js
+++ app/router/router.js
@@ -2,8 +2,8 @@ const controller = require('../controller/controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/api/markers', controller.getMarkers);
-router.get('/api/marker/:id', controller.getMarkerData);
-router.post('/api/search', controller.searchData);
+router.get('/markers', controller.getMarkers);
+router.get('/marker/:id', controller.getMarkerData);
+router.post('/search', controller.searchData);
 
 module.exports = router;
