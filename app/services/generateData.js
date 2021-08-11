ea574a52340646401bf4d0d2a9fff7607c416484
GuillaumePavic <guillaume.pavic.56@gmail.com>
2021-08-12 00:07:39 +0200

    refactored generateData

--- app/services/generateData.js
+++ app/services/generateData.js
@@ -5,16 +5,16 @@ const { formatTime } = require('../utils/date');
 
 
 async function generateData(lat, lng) {
-		let SGData = await dataMapper.fetchStormGlass(lat, lng);
+	let SGData = await dataMapper.fetchStormGlass(lat, lng);
 
-		for(hr in SGData) {
-			for(data in SGData[hr]) {
-				if(data !== 'time') SGData[hr][data] = calculAverage(SGData[hr][data]);
-				if(data === 'time') SGData[hr][data] = formatTime(SGData[hr][data]);
-			}
-			
-			SGData[hr].suggestions = generateSuggestions(SGData[hr]);
+	for(hr in SGData) {
+		for(data in SGData[hr]) {
+			if(data !== 'time') SGData[hr][data] = calculAverage(SGData[hr][data]);
+			if(data === 'time') SGData[hr][data] = formatTime(SGData[hr][data]);
 		}
+	
+		SGData[hr].suggestions = generateSuggestions(SGData[hr]);
+	}
 		
 return SGData;
 }
