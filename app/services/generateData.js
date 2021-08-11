53ae96f0a38b8255d3d75cfd832f6e9f9f6b8fdf
GuillaumePavic <guillaume.pavic.56@gmail.com>
2021-08-12 00:01:51 +0200

    refactored generateData

--- app/services/generateData.js
+++ app/services/generateData.js
@@ -10,11 +10,10 @@ async function generateData(lat, lng) {
 		for(hr in SGData) {
 			for(data in SGData[hr]) {
 				if(data !== 'time') SGData[hr][data] = calculAverage(SGData[hr][data]);
-
-				SGData[hr].suggestions = generateSuggestions(SGData[hr]);
-
 				if(data === 'time') SGData[hr][data] = formatTime(SGData[hr][data]);
 			}
+			
+			SGData[hr].suggestions = generateSuggestions(SGData[hr]);
 		}
 		
 return SGData;
