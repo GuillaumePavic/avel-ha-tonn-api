989197fac49cd9b7d205f472c9067e976d3e33e9
GuillaumePavic <guillaume.pavic.56@gmail.com>
2021-08-29 11:53:48 +0200

    added validation in user model

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,14 +1,32 @@
 const mongoose = require('mongoose');
 const {markerSchema} = require('./markerModel');
 
+const validateRegex = (value) => {
+    return /^[ a-zA-Z0-9\-_]{1,50}$/.test(value);
+};
+
+const customValidator = [validateRegex, 'Uh oh, {PATH} ne peut contenir que des lettres, des chiffres, des espaces ainsi que les symboles - et _'];
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
         trim: true,
+        validate: {
+            validator: function(v) {
+              return /^[ a-zA-Z0-9\-_]{1,50}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid name!`
+        },
+        required: true,
     },
     email: {
         type: String,
+        validate: {
+            validator: function(v) {
+              return /^[A-Za-z0-9+_.-]+@(.+)$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        },
         required: true,
     },
     password: {
